4ba3468243750cd182c1496c556eab5ef11312c3
ClaudioBrasilia <claudiobrasilia13@gmail.com>
2026-07-03 08:14:05 -0300

    Hide navigation in Register page layout

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -72,7 +72,7 @@ const Register: React.FC = () => {
   };
   
   return (
-    <PageLayout>
+    <PageLayout hideNav>
       <div className="flex flex-col items-center justify-center py-8 px-4">
         <div className="w-full max-w-md space-y-6">
           <div className="text-center">
